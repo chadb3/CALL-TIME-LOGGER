aaa35dadd9af271d2d20311249a6a19e94508524
chad <chadb78999@gmail.com>
2022-08-06 01:12:28 -0500

    worked on Call.py the object not the original.

--- src/Call.py
+++ src/Call.py
@@ -1,4 +1,17 @@
-import time
+from datetime import *
 
 class Call:
-
+    def __int__(this):
+        this.callDate = ""
+        this.callTime = ""
+        this.callEndTime=""
+        this.callEndDate=""
+        this.callNumber = ""
+    def setStartTime(this):
+        # need to split out
+        this.callDate = datetime.now()
+        this.callTime = datetime.now()
+    def setCallNumber(this, numberIn):
+        this.callNumber = numberIn
+    def zTestPrintCallNum(this):
+        print(this.callNumber)
