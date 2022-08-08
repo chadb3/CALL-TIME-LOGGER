a78d03a888af0b38bda41df722ff48dc27bff4eb
chad <chadb78999@gmail.com>
2022-08-08 01:51:11 -0500

    formatting Call. Added Some comments

--- src/Call.py
+++ src/Call.py
@@ -7,16 +7,24 @@ class Call:
         this.callEndTime=""
         #this.callEndDate=""
         this.callNumber = ""
+    # sets the call start time
+    # note: also sets the end time.
+    # set the end time to the start time
     def setStartTime(this):
         # need to split out
         #this.callDate = datetime.now()
         this.callTime = datetime.now()
         this.callEndTime = this.callTime
+    # sets the call number
     def setCallNumber(this, numberIn):
         this.callNumber = numberIn
-    def zTestPrintCallNum(this):
-        print(this.callNumber)
-    def zTestPrintCallStartAndEndTime(this):
-        print("start: {}\nend: {}".format(this.callTime,this.callEndTime))
+    # sets the end time.
     def setEndTime(this):
         this.callEndTime=datetime.now()
+    # used to see if it prints the correct call number
+    # also used to test to see if call numbers hold if added to a list 
+    def zTestPrintCallNum(this):
+        print(this.callNumber)
+    #used to test if start and endtimes are working correctly
+    def zTestPrintCallStartAndEndTime(this):
+        print("start: {}\nend: {}".format(this.callTime,this.callEndTime))
