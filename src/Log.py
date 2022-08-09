d35a673923a3d0cac4ce190e06e8c7b8c58bfb07
Chad Bachman <chadb78999@gmail.com>
2022-08-09 05:54:49 -0500

    More work on Log

--- src/Log.py
+++ src/Log.py
@@ -1,14 +1,17 @@
+from random import randint
 class Log:
     def __init__(this):
         #filepath to history file
+        this.sessionStartStr = "{} - SESSION START - {}"
+        this.sessionEndStr ="{} - SESSION END - {}"
         this.filepath = "../data/"
         this.filename ="history.txt"
         #datetime COMMAND: commandIn
-        this.commandStr = "{} COMMAND: {}"
+        this.commandStr = "{} - COMMAND: {}"
         #datetime SYSTEM: \"SYSTEM OUTPUT\"
-        this.systemStr = "{} SYSTEM: {}"
+        this.systemStr = "{} - SYSTEM: {}"
         #datetime INPUT: "user input that wasn't a command or other."
-        this.userInputStr = "{} INPUT: {}"
+        this.userInputStr = "{} - INPUT: {}"
     def logCommand(this, dateTimeIn, commandIn):
         this._writeToFile(this.commandStr.format(dateTimeIn, commandIn))
         return 0
@@ -19,9 +22,21 @@ class Log:
         this._writeToFile(this.userInputStr.format(dateTimeIn,userInputIn))
         return 0
     def _startLogging(this, dateTimeIn):
+        #datetime - SESSION START - this.filename
         return 0
     def endLogging(this, dateTimeIn):
-        return 0
+        #datetime - SESSION END - :)
+        #datetime - SESSION END - :^)
+        randNum=randint(1,5999)
+        endSessionStr = ""
+        if(randNum%3==0):
+            endSessionStr= ":^)"
+        else:
+            endSessionStr= ":)"
+        this._writeToFile(this.sessionEndStr.format(dateTimeIn,endSessionStr))
+        return endSessionStr
     def _writeToFile(this, thingIn):
         file =open(this.filepath+this.filename,'a')
+        file.write(thingIn+"\n")
+        file.close()
         return 0
