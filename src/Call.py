30fb2dc355eafcf7e89eb5cdb6386b4342e1c49f
chad <chadb78999@gmail.com>
2022-08-15 01:56:44 -0500

    worked on call

--- src/Call.py
+++ src/Call.py
@@ -3,10 +3,10 @@ from datetime import datetime, timedelta
 class Call:
     def __init__(this, callNumber=-1):
         #this.callDate = ""
-        this.callTime = ""
-        this.callEndTime=""
+        this._callTime = ""
+        this._callEndTime=""
         #this.callEndDate=""
-        this.callNumber = callNumber
+        this._callNumber = callNumber
         this.setStartTime()
     # sets the call start time
     # note: also sets the end time.
@@ -14,21 +14,21 @@ class Call:
     def setStartTime(this):
         # need to split out
         #this.callDate = datetime.now()
-        this.callTime = datetime.now()
-        this.callEndTime = this.callTime
+        this._callTime = datetime.now()
+        this._callEndTime = this._callTime
     # sets the call number
     def setCallNumber(this, numberIn):
-        this.callNumber = numberIn
+        this._callNumber = numberIn
     # sets the end time.
     def setEndTime(this):
-        this.callEndTime=datetime.now()
+        this._callEndTime=datetime.now()
     # used to see if it prints the correct call number
     # also used to test to see if call numbers hold if added to a list 
     def zTestPrintCallNum(this):
-        print(this.callNumber)
+        print(this._callNumber)
     #used to test if start and endtimes are working correctly
     def zTestPrintCallStartAndEndTime(this):
-        print("start: {}\nend: {}".format(this.callTime,this.callEndTime))
+        print("start: {}\nend: {}".format(this._callTime,this._callEndTime))
     #This is for adjusting the end time of a call
     #Just in case the user forgets to manually end the call time. 
     #WIP
@@ -42,13 +42,18 @@ class Call:
         return 0
     # used to ajust call number. To increse callNumber by 1.
     def increaseCallNum(this):
-        if(this.callNumber>0):
-           this.callNumber+=1
+        if(this._callNumber>0):
+           this._callNumber+=1
         else:
            print("set call number")
     # used to adjust call number. To decrease callNumber by 1.
     def decreaseCallNum(this):
-        if(this.callNumber>1):
-           this.callNumber-=1
+        if(this._callNumber>1):
+           this._callNumber-=1
         else:
            print("call number < 1")
+    def __str__(this):
+        if(this._callTime==this._callEndTime):
+            return " Call: {}\tDate: {}\tTime: {}\tDuration: 0\t".format(this._callNumber,this._callTime.date(),str(this._callTime.time())[0:8])
+        else:
+            return " Call: {}\tDate: {}\tTime: {}\tDuration: {}\t".format(this._callNumber,this._callTime.date(),str(this._callTime.time())[0:8],str(this._callEndTime-this._callTime)[0:7])
--- src/Command.py
+++ src/Command.py
@@ -3,21 +3,6 @@ from datetime import *
 #from datetime import timedelta, datetime, date
 from random import randint
 import re
-# proof of concept
-#a=True
-#i = 0
-#while(a):
-# n=input("A: ")
-# i+=1
-# if(i==10):
-#  a=False
-# print(n)
-# end of proof of concept
-# Notes: commands for "call"
-# Y - new call
-# yes - new call
-# new call - new call
-#:
 # XML notes: import xml.etree.ElementTree as ET
 class Command:
 	def __init__(this,command="",command_type="",command_options=[],command_alias=[],command_help=""):
@@ -32,25 +17,9 @@ class Command:
 	def getCommand(this):
 		return this._command
 	def getCommandType(this):
-		return this_.command_type
+		return this._command_type
 	def getCommandOptions(this):
 		return this._command_options
 	def getCommandHelp(this):
 		return this._command_help
-	# All of the below works, but I am going in a different direction now.
-	#def listOfCommands(this):
-		#for command in this.commands:
-			#print(command)
-	#def addCommand(this, command):
-		#this.commands.append(command)
-	#def zAppendCommandTest(this):
-		#this.commands.append("TEST 1")
-		#this.commands.append("TEST 2")
-		#this.commands.append("TEST 3")
-	#def _getDateTime(this):
-		#return datetime.now()
-	#def zDateTimeTest(this):
-		#return this._getDateTime()
-	#def printHelp(this, command):
-		#print("Getting help for {}")
-		#return 0
+
