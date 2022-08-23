9d928d572fd81499e245e83d24b5db5e45cfb8a3
chad <chadb78999@gmail.com>
2022-08-23 00:13:03 -0500

    added function to get time to first call)

--- src/callManager.py
+++ src/callManager.py
@@ -1,7 +1,10 @@
 from Call import *
+from datetime import datetime
 
 class callManager:
     def __init__(this):
+        this.initTime=datetime.now()
+        this.timeFirstCall=0
         this.callList = []
         this.callCount = 0
     def newCall(this):
@@ -12,11 +15,18 @@ class callManager:
                 this.callList[this.callCount-1].setEndTime()
         this.callCount+=1
         this.callList.append(Call(this.callCount))
+        if(len(this.callList)==1 and this.timeFirstCall==0):
+            this.timeFirstCall=datetime.now()
     def printCalls(this):
         for call in this.callList:
             print(call)
     def askDuration(this):
-        print("asking current delta/Duration")
+        #print("asking current delta/Duration")
         lenList=len(this.callList)
         if(lenList>0):
             this.callList[lenList-1].currentDelta()
+    def getTimeToFirstCall(this):
+        print("Time to first call")
+        if(len(this.callList)==1):
+            #print("LEN=1")
+            print("Time to First: {}".format(str(this.timeFirstCall-this.initTime)[0:7]))
