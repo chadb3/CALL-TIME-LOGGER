730f4b6ec24694cfdab57bf97fd10a2c5faf8011
chad <chadb78999@gmail.com>
2022-08-29 05:51:29 -0500

    worked on comments in callManager. starting to set up call-log.py

--- call-log.py
+++ call-log.py
@@ -2,13 +2,11 @@ from src import *
 from time import sleep
 
 def main():
-    print("TEST")
-    a = Call(1)
-    sleep(60)
-    b = Call(2)
-    sleep(60)
-    c = Call(3)
-    print("{}\n{}\n{}\n".format(a,b,c))
+    print("STARTING")
+    true=True
+    while(true):
+        usrInput=input("call-logger>> ")
+        0
     return 0
 
 if __name__ == "__main__":
--- src/callManager.py
+++ src/callManager.py
@@ -2,11 +2,13 @@ from Call import *
 from datetime import datetime
 
 class callManager:
+    # constructor
     def __init__(this):
         this.initTime=datetime.now()
         this.timeFirstCall=0
         this.callList = []
         this.callCount = 0
+    # add a new call to the list
     def newCall(this):
         if(this.callCount>0):
             #debug
@@ -17,24 +19,29 @@ class callManager:
         this.callList.append(Call(this.callCount))
         if(len(this.callList)==1 and this.timeFirstCall==0):
             this.timeFirstCall=datetime.now()
+    # prints the list of calls
     def printCalls(this):
         for call in this.callList:
             print(call)
+    #"Ends" current call - sets the end time for the call. 
     def endCurrentCall(this):
         if(len(this.callList)>0):
             this.callList[len(this.callList)-1].setEndTime()
         else:
             print("No Calls to End")
+    # shows the duration of the current call
     def askDuration(this):
         #print("asking current delta/Duration")
         lenList=len(this.callList)
         if(lenList>0):
             this.callList[lenList-1].currentDelta()
+    # gets the time to the first call from initilization 
     def getTimeToFirstCall(this):
         print("Time to first call")
         if(len(this.callList)==1):
             #print("LEN=1")
             print("Time to First: {}".format(str(this.timeFirstCall-this.initTime)[0:7]))
+    # function to remove a call from the list
     def removeCall(this, callNumber):
         print("Attempting to remove call: {}".format(callNumber))
         numCalls=len(this.callList)
@@ -47,11 +54,20 @@ class callManager:
                 print(this.callList[i])
         else:
             print("NO CALLS TO REMOVE")
+    # adjust endtime of call.
+    # WIP.
+    # example use case: forget to "end" the call, but know when it was ended.
     def adjustTimeOfCall(this,callNumber,timeIN):
         print("WIP")
         print("Adjusting time of Call: {}".format(callNumber))
+    # prints the list of the calls with the end times rather than the durations
     def printCallsAlternate(this):
         for call in this.callList:
             call.print_alternate()
+    # prints the call count.
     def printCallCount(this):
         print(len(this.callList))
+    # WIP command set.
+    # trying to figure out a way to create commands programmatically
+    def commandBuilder(this):
+        print("Building Commands for phoneNumberManager")
