7e5ea7625232e8d3778b9b486be7f1f8379b0c95
chad <chadb78999@gmail.com>
2022-09-11 07:27:53 -0500

    worked on call-log

--- call-log.py
+++ call-log.py
@@ -2,6 +2,7 @@ from src.callManager import *
 from src.commandController import *
 from time import sleep
 import sqlite3
+from random import randint
 def main():
     filepath="/config/commands.txt"
     callM=callManager()
@@ -12,6 +13,11 @@ def main():
         usrInput=input("call-logger>> ")
         usrInput=usrInput.upper()
         if(usrInput=="EXIT" or usrInput=="EXIT()"):
+            ran=randint(0,99999)
+            #easter egg
+            if(ran==1001 or ran==2001 or ran==3001 or ran==4001 or ran==5001 or ran==6001 or ran==7001 or ran==8001 or ran==9001 or ran==10001):
+                print("Bye\n:^)")
+            print(ran)
             break
         elif(usrInput=="Y" or usrInput=="NEW CALL" or usrInput =="YES"):
             callM.newCall()
